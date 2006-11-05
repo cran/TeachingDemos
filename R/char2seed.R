12e4bb69150513d5ee131ac7d512e16f912a3a02
Greg Snow <greg.snow@ihc.com>
2006-11-05 03:24:00 +0000

    version 1.4

---
+++ R/char2seed.R
@@ -0,0 +1,19 @@
+char2seed <- function(x,set=TRUE,...){
+
+	tmp <- c(0:9,0:25,0:25)
+	names(tmp) <- c(0:9,letters,LETTERS)
+
+	x <- gsub("[^0-9a-zA-Z]","",as.character(x))
+
+	xsplit <- tmp[ strsplit(x,'')[[1]] ]
+
+	seed <- sum(rev( 7^(seq(along=xsplit)-1) ) * xsplit)
+
+	if(set){
+		set.seed(seed,...)
+		return(invisible(seed))
+	} else {
+		return(seed)
+	}
+}
+
---
+++ R/squishplot.R
@@ -0,0 +1,37 @@
+squishplot <- function(xlim,ylim,asp=1){
+  if(length(xlim) < 2) stop('xlim must be a vector of length 2')
+  if(length(ylim) < 2) stop('ylim must be a vector of length 2')
+
+  tmp <- par(c('plt','pin','xaxs','yaxs'))
+
+  if( tmp$xaxs == 'i' ){ # not extended axis range
+	xlim <- range(xlim)
+  } else { # extended range
+	tmp.r <- diff(range(xlim))
+	xlim <- range(xlim) + c(-1,1)*0.04*tmp.r
+  }
+
+  if( tmp$yaxs == 'i' ){ # not extended axis range
+	ylim <- range(ylim)
+  } else { # extended range
+	tmp.r <- diff(range(ylim))
+	ylim <- range(ylim) + c(-1,1)*0.04*tmp.r
+  }
+
+
+  tmp2 <- (ylim[2]-ylim[1])/(xlim[2]-xlim[1])
+
+  tmp.y <- tmp$pin[1] * tmp2 * asp
+
+  if(tmp.y < tmp$pin[2]){ # squish vertically
+	par(pin=c(tmp$pin[1], tmp.y))
+	par(plt=c(tmp$plt[1:2], par('plt')[3:4]))
+  } else { # squish horizontally
+	tmp.x <- tmp$pin[2]/tmp2/asp
+	par(pin=c(tmp.x, tmp$pin[2]))
+	par(plt=c(par('plt')[1:2], tmp$plt[3:4]))
+  }
+
+  return(invisible(tmp['plt']))
+}
+
